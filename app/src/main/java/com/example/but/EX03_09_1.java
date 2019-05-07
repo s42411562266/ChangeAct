862155986129b19782f73b962efe7f490b171124
你的名字 <email>
2019-05-07 12:43:04 +0800

    now can change textview color!

--- app/src/main/java/com/example/but/EX03_09_1.java
+++ app/src/main/java/com/example/but/EX03_09_1.java
@@ -1,10 +1,15 @@
 package com.example.but;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
 public class EX03_09_1 extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,16 @@ public class EX03_09_1 extends AppCompatActivity {
                 EX03_09_1.this.finish();
             }
         } ) ;
+
+        Button bC=(Button)findViewById(R.id.button2);
+        final TextView text=(TextView)findViewById(R.id.text1);
+        bC.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Random R=new Random();
+                int size=35;
+                text.setTextColor(Color.rgb(R.nextInt(),R.nextInt(),R.nextInt()));
+                text.setTextSize(size);
+            }
+        });
     }
 }
